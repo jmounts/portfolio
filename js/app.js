e66e4d0944820a63ddb83bab04c3684aa873a213
Jeff Mounts <mountzer@gmail.com>
2014-04-19 11:07:29 -0500

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$('a').click( fucntion(event){
+  event.preventDefault();
+};
