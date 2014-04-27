ccab5cceb7ab9c51b7f22945a296834ccc01c6cc
kurtcunningham <cunningham.kurt@gmail.com>
2014-04-27 10:53:39 -0500

    added side view jquery and made it animate on side in and side out

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+
+//PAGE SIDE IN VIEW
+
+ $(function(){
+    var $page_trigger = $(".page-trigger")
+
+    $page_trigger.on("click", function(event){
+      
+      var page_trigger_id = $(this).data('page-trigger-id');
+
+      event.preventDefault();
+     $(".main-view").hide();
+     $('#page-trigger-id-' + page_trigger_id).show().addClass('animated slideInRight');;
+    });
+
+    $(".side-view .main-view-trigger").on("click", function(event){
+      event.preventDefault();
+      $(".side-view").hide();
+      $(".main-view").show().addClass('animated slideInLeft');;
+    });
+
+  });
